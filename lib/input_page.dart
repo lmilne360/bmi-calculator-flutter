14f6ccb02dfd3b1c751cafa6ce527b8d6ff9d68c
Laurent Milne <laurent.milne@bottlerocketstudios.com>
2020-04-29 02:35:19 -0500

    Added child property to reusableCardComponent

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -55,13 +55,15 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.color});
+  ReusableCard({@required this.color, this.cardChild});
 
   final Color color;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
